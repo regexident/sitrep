b674fe24b5f35ba6554f68fa62ada70a412e030d
Vincent Esche <regexident@gmail.com>
2025-06-05 10:32:14 +0200

    Fix clippy warnings

--- benches/partial_report.rs
+++ benches/partial_report.rs
@@ -1,4 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, Criterion};
 
 use sitrep::{
     test_utils::{make_hierarchy, make_stand_alone},
